320e746ed683b684d427fd4598f2ff42067f529b
vinc <794914601@qq.com>
2017-02-01 02:50:21 +0800

    2017.1.31

---
+++ src/main/java/com/qiuming/entity/TestBean.java
@@ -0,0 +1,30 @@
+package com.qiuming.entity;
+
+public class TestBean {
+
+	public String beanPro;
+	public long id;
+	public String name;
+	
+	public TestBean(){
+	}
+	
+	public String getBeanPro() {
+		return beanPro;
+	}
+	public void setBeanPro(String beanPro) {
+		this.beanPro = beanPro;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
