2f7d9b8839aa16d4243349c0b576b1a62bcd2c18
VincentField <vincent_field@foxmail.com>
2017-02-04 15:20:21 +0800

    ..

--- src/main/java/com/qiuming/controller/NetDiskController.java
+++ src/main/java/com/qiuming/controller/NetDiskController.java
@@ -7,20 +7,27 @@ import java.io.InputStreamReader;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.qiuming.dao.CommentNoteDao;
+import com.qiuming.entity.CommentNote;
 import com.qiuming.entity.TestBean;
 
 @Controller
 public class NetDiskController {
 
+//	@Autowired
+//	CommentNoteDao cnDao;
+	
 	@ResponseBody
 	@RequestMapping(value="framework")
-	public Object testFramework(TestBean tb){
-		
-		return tb;
+	public Object testFramework(){
+//		CommentNote note = cnDao.selectByPrimaryKey(1);
+//		return note;
+		return "sss";
 	}
 	
 }
